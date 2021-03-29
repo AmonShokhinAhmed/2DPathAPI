3a005719ed21d11e32b41e58788ba8c86691f7bf
Amon <Amon.Shokhin.Ahmed@gmail.com>
2021-03-30 01:11:05 +0200

    Tweak clang-format to indent code inside namespaces

--- 2DPathAPI/src/pathfinder.h
+++ 2DPathAPI/src/pathfinder.h
@@ -2,31 +2,31 @@
 #include <vector>
 /// The namespace for the whole 2DPathAPI project.
 namespace PathAPI {
-class Node;
-/// The heart of the PathAPI, that should find the path for the user.
-/**
- * This class handles pretty much everything the user wants from the PathAPI.
- */
-class PathFinder {
-  public:
-    /// Default Constructor
+    class Node;
+    /// The heart of the PathAPI, that should find the path for the user.
     /**
-     * Will probably be replaced soon
+     * This class handles pretty much everything the user wants from the PathAPI.
      */
-    PathFinder();
-    /// Adding a node to the pathfinders current map.
-    /**
-     * This is the only way a node can be created. Because creating a node in a vacuum does not make
-     * any sense. It also ensures that the node gets the correct id value.
-     */
-    Node *AddNode(const std::vector<uint32_t> &connections);
+    class PathFinder {
+      public:
+        /// Default Constructor
+        /**
+         * Will probably be replaced soon
+         */
+        PathFinder();
+        /// Adding a node to the pathfinders current map.
+        /**
+         * This is the only way a node can be created. Because creating a node in a vacuum does not
+         * make any sense. It also ensures that the node gets the correct id value.
+         */
+        Node *AddNode(const std::vector<uint32_t> &connections);
 
-    /// Gets a node from the map corresponding to the id
-    Node *GetNode(uint32_t id);
-    /// Getter for the size of the Map
-    uint32_t GetMapSize();
+        /// Gets a node from the map corresponding to the id
+        Node *GetNode(uint32_t id);
+        /// Getter for the size of the Map
+        uint32_t GetMapSize();
 
-  private:
-    std::vector<Node *> map;
-};
+      private:
+        std::vector<Node *> map;
+    };
 } // namespace PathAPI
