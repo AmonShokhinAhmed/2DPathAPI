30b91e88fa2eadcef3fab27c304f35f0084b1824
AmonShokhinAhmed <32668336+AmonShokhinAhmed@users.noreply.github.com>
2021-04-07 10:17:56 +0000

    Deploying to gh-pages from @ AmonShokhinAhmed/2DPathAPI@2764ff0ffa12c663e2e428f7cd41f02e4b11d996 🚀

--- html/search/functions_2.js
+++ html/search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['findfastpath_47',['FindFastPath',['../class_path_a_p_i_1_1_path_finder.html#abcb1e94b42c3f510b6bad3c965fe0936',1,'PathAPI::PathFinder::FindFastPath(const Vector2 &amp;start, const Vector2 &amp;end, bool &amp;pathFound)'],['../class_path_a_p_i_1_1_path_finder.html#ac215a4a03792628778035f9572db95aa',1,'PathAPI::PathFinder::FindFastPath(const std::vector&lt; Vector2 &gt; &amp;positions, bool &amp;pathFound)']]],
-  ['findoptimizedpath_48',['FindOptimizedPath',['../class_path_a_p_i_1_1_path_finder.html#a214b8a86ac6fe641a731c0d965294070',1,'PathAPI::PathFinder::FindOptimizedPath(const Vector2 &amp;start, const Vector2 &amp;end, bool &amp;pathFound)'],['../class_path_a_p_i_1_1_path_finder.html#a4a70a0e1b83afedbb464f365d4ef1ac4',1,'PathAPI::PathFinder::FindOptimizedPath(const std::vector&lt; Vector2 &gt; &amp;, bool &amp;pathFound)']]]
+  ['findoptimizedpath_48',['FindOptimizedPath',['../class_path_a_p_i_1_1_path_finder.html#a214b8a86ac6fe641a731c0d965294070',1,'PathAPI::PathFinder::FindOptimizedPath(const Vector2 &amp;start, const Vector2 &amp;end, bool &amp;pathFound)'],['../class_path_a_p_i_1_1_path_finder.html#ac23d2a5a6ceca375a7b20be4aa0de956',1,'PathAPI::PathFinder::FindOptimizedPath(const std::vector&lt; Vector2 &gt; &amp;positions, bool &amp;pathFound)']]]
 ];
